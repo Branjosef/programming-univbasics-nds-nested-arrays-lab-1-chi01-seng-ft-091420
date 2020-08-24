5673175101fca9783747cb5cec38eaae237c2760
Brandon Fonnemann <branjosef@gmail.com>
2020-08-24 14:36:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -23,22 +23,20 @@ def assembled_matrix
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
   
   assembled_matrix = [
+            CONVENTIONAL_PRODUCE = [
+          "Grapefruit",
+          "Pineapple",
+          "Oranges",
+          "Watermelon",
+          "Eggplant"
+        ],
             ORGANIC_PRODUCE = [
           "Strawberries",
           "Potatoes",
           "Grapes",
           "Avocadoes",
           "Asparagus"
-        ],
-        
-        CONVENTIONAL_PRODUCE = [
-          "Grapefruit",
-          "Pineapple",
-          "Oranges",
-          "Watermelon",
-          "Eggplant"
         ]
-    
     ]
   
 end
